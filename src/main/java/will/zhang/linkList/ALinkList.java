b40d6bc96ab25d68aa3819884ac35e135b9e3511
349071347@qq.com <349071347@qq.com>
2018-05-02 20:41:14 +0800

    add linkList and update readme

---
+++ src/main/java/will/zhang/linkList/ALinkList.java
@@ -0,0 +1,37 @@
+package will.zhang.linkList;
+
+/**
+ * @Author will
+ * @Date 2018/5/2 0002 下午 8:31
+ **/
+public class ALinkList<E> {
+
+    /**
+     * 节点
+     * 链表的内部类
+     */
+    private class Node {
+        //数据存放在节点中
+        public E e;
+        //下一个节点
+        public Node next;
+
+        public Node(E e, Node next){
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e){
+            this(e, null);
+        }
+
+        public Node(){
+            this(null, null);
+        }
+
+        @Override
+        public String toString() {
+            return e.toString();
+        }
+    }
+}
