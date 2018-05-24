b6e897af63effd497c61f153eec980550f821432
will <349071347@qq.com>
2018-05-24 11:31:53 +0800

    update unionFind

--- README.md
+++ README.md
@@ -73,3 +73,4 @@
 4. [基于size的优化](https://github.com/l81893521/basic-data-structure/blob/master/src/main/java/will/zhang/unionFind/CUnionFind.java)
 5. [基于rank的优化](https://github.com/l81893521/basic-data-structure/blob/master/src/main/java/will/zhang/unionFind/DUnionFind.java)
 6. [路径压缩](https://github.com/l81893521/basic-data-structure/blob/master/src/main/java/will/zhang/unionFind/EUnionFind.java)
+6. [路径压缩2](https://github.com/l81893521/basic-data-structure/blob/master/src/main/java/will/zhang/unionFind/FUnionFind.java)
---
+++ src/main/java/will/zhang/unionFind/FUnionFind.java
@@ -0,0 +1,90 @@
+package will.zhang.unionFind;
+
+/**
+ * 第五版的并查集(路径压缩2)
+ * 在Find的过程中, 直接把树的高度降低到2
+ */
+public class FUnionFind implements UF {
+
+    //parnet[i]代表它所指向的节点
+    private int[] parent;
+    //rank[i]表示以i为根的集合所表示的树的层数
+    private int[] rank;
+
+    public FUnionFind(int size){
+        parent = new int[size];
+        rank = new int[size];
+
+        //初始化的时候每一个节点都指向自己, 也就是每一个节点都是一棵独立的树
+        for (int i = 0; i < size; i++) {
+            parent[i] = i;
+            //初始化的层数都为1
+            rank[i] = 1;
+        }
+    }
+
+    @Override
+    public int getSize() {
+        return parent.length;
+    }
+
+    /**
+     * 查看p和q是否属于同一个集合
+     * 时间复杂度O(h), p是树的高度
+     * @param p
+     * @param q
+     * @return
+     */
+    @Override
+    public boolean isConnected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    /**
+     * 合并元素p和元素q所属的集合
+     * 时间复杂度O(h), p是树的高度
+     * @param p
+     * @param q
+     */
+    @Override
+    public void unionElements(int p, int q) {
+        int pRoot = find(p);
+        int qRoot = find(q);
+
+        if(pRoot == qRoot) return;
+
+        //主要优化是在合并两个集合的时候
+        //合并的时候需要考虑到两个集合所属的树的高度, 将rank小的树合并到rank高的树下()
+        //而非一味的往同一个方式合并两个集合, 这样子在坏的情况下树的高度会变得非常高, 甚至退化成为一个链表
+        if(rank[pRoot] < rank[qRoot]){
+            parent[pRoot] = qRoot;
+            //rank不用维护, 合并后并不会导致rank[qRoot]增加高度
+        }else if(rank[qRoot] < rank[pRoot]){
+            parent[qRoot] = pRoot;
+            //依然不用维护rank, , 合并后并不会导致rank[pRoot]增加高度
+        }else{  //rank[qRoot] = rank[pRoot]
+            //同等高度谁指向谁并不重要
+            parent[qRoot] = pRoot;
+            //只要相等, 才会导致高度增加, 而且只会增加1
+            rank[pRoot] += 1;
+        }
+    }
+
+    /**
+     * 查询p所对应的集合编号
+     * 时间复杂度O(h), p是树的高度
+     * @param p
+     * @return
+     */
+    private int find(int p){
+        if(p < 0 || p >= parent.length){
+            throw new IllegalArgumentException("p is out of bound");
+        }
+        //改成使用递归的写法
+        //find的时候, 把树的高度直接压缩到2
+        if(p != parent[p]){
+            parent[p] = find(parent[p]);
+        }
+        return parent[p];
+    }
+}
